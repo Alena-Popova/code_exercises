81ba61b0e96b847da6cc5e2deeff315df9857e35
alenapopova <popova-alena1995@mail.ru>
2024-07-30 11:58:42 +0300

    123

--- src/main/java/org/example/amazon/CheckStraightLineImpl.java
+++ src/main/java/org/example/amazon/CheckStraightLineImpl.java
@@ -3,6 +3,7 @@ package org.example.amazon;
 /**
  * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
  */
+//123
 public class CheckStraightLineImpl
 {
     /**
